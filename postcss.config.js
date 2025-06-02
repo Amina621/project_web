e4f55681506536c4386cb389c47513c75b573937
EliteBook 840 G4 <aminamouini4@gmail.com>
2025-06-03 00:58:02 +0100

    create header conponents

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// ✅ Corrigé pour que ça fonctionne avec Vite et Node.js
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}", // Scanne tous les fichiers React pour générer les classes utilisées
