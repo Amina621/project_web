6422d806d31d105260c8c6d3a11113ab5231f54d
EliteBook 840 G4 <aminamouini4@gmail.com>
2025-06-16 00:59:30 +0100

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,6 +9,9 @@ import {Provider} from "react-redux"
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+   < persistGate loading={null} persistor={persistor}>
+
     <App />
+    </persistGate>
   </Provider>,
 )
